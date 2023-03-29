26c2697631f1fbc6c9c164edf235517697984eb8
brianmanderson <markba122@gmail.com>
2023-03-29 13:58:36 -0300

    got the same result as the python file!

--- Program.cs
+++ Program.cs
@@ -10,11 +10,17 @@ using Numpy;
 using SitkImage = itk.simple.Image;
 using PixelId = itk.simple.PixelIDValueEnum;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Reflection;
 
 namespace FindingStablePointsCSharp
 {
     internal class Program
     {
+        public static int RoundUpToOdd(double f)
+        {
+            return (int)(Math.Ceiling(f) / 2) * 2 + 1;
+        }
         static SitkImage copy_base_information(SitkImage bare_image, SitkImage base_image)
         {
             bare_image.CopyInformation(base_image);
@@ -22,6 +28,7 @@ namespace FindingStablePointsCSharp
         }
         static Image return_image_from_array(NDarray np_array)
         {
+            np_array = np_array.astype(np.float32);
             int len = np_array.shape[0] * np_array.shape[1] * np_array.shape[2];
             SitkImage outImage = new SitkImage((uint)np_array.shape[2], (uint)np_array.shape[1], (uint)np_array.shape[0],PixelId.sitkFloat32);
             IntPtr outImageBuffer = outImage.GetBufferAsFloat();
@@ -47,19 +54,25 @@ namespace FindingStablePointsCSharp
             dose_np = np.reshape(dose_np, output_shape);
             return dose_np;
         }
+        static void write_image(SitkImage image)
+        {
+            SimpleITK.WriteImage(image, @"C:\Users\markb\Modular_Projects\FindingStablePoints\compared.nii.gz");
+        }
         static void Main(string[] args)
         {
             string dose_file = @"C:\Users\markb\Modular_Projects\FindingStablePoints\dose.nii.gz";
             var reader = new ImageFileReader();
-            double limit = 0.9;
+            double limit = 0.7;
             reader.SetFileName(dose_file);
             reader.ReadImageInformation();
             SitkImage dose_handle = reader.Execute();
+            VectorDouble voxel_size = dose_handle.GetSpacing();
+
+            List<float> camera_dimensions = new List<float> { 3, 5, 3 };
+            List<int> voxels_needed = new List<int> { RoundUpToOdd(camera_dimensions[0] / voxel_size[0]) ,
+            RoundUpToOdd(camera_dimensions[1] / voxel_size[1]), RoundUpToOdd(camera_dimensions[2] / voxel_size[2])};
+            
             NDarray dose_np = return_array_from_image(dose_handle);
-            SitkImage new_dose_handle = return_image_from_array(dose_np);
-            new_dose_handle = copy_base_information(new_dose_handle, dose_handle);
-            SimpleITK.WriteImage(new_dose_handle, @"C:\Users\markb\Modular_Projects\FindingStablePoints\dose_compared.nii.gz");
-            NDarray max_dose = np.max(dose_np);
             ConnectedComponentImageFilter Connected_Component_Filter = new ConnectedComponentImageFilter();
             ConnectedThresholdImageFilter Connected_Threshold = new ConnectedThresholdImageFilter();
             Connected_Threshold.SetLower(1);
@@ -68,12 +81,45 @@ namespace FindingStablePointsCSharp
             /*
             Next, identify each independent segmentation in both
             */
-            NDarray masked_np = (dose_np >= limit * max_dose).astype(np.int32);
-            //var connected_image_handle = Connected_Component_Filter.Execute(base_mask);
+            NDarray masked_np = (dose_np >= limit * np.max(dose_np));
+            SitkImage base_mask = return_image_from_array(masked_np);
+            base_mask = SimpleITK.Cast(base_mask, PixelId.sitkInt32);
 
-            var RelabelComponentFilter = new RelabelComponentImageFilter();
-            //var connected_image = RelabelComponentFilter.Execute(connected_image_handle);
-            //truth_stats.Execute(connected_image);
+            SitkImage connected_image_handle = Connected_Component_Filter.Execute(base_mask);
+
+            RelabelComponentImageFilter RelabelComponentFilter = new RelabelComponentImageFilter();
+            SitkImage connected_image = RelabelComponentFilter.Execute(connected_image_handle);
+            write_image(connected_image);
+            truth_stats.Execute(connected_image);
+            List<VectorUInt32> bounding_boxes = new List<VectorUInt32>();
+            foreach (long label in truth_stats.GetLabels())
+            {
+                bounding_boxes.Add(truth_stats.GetBoundingBox(label));
+            }
+            foreach (VectorUInt32 bounding_box in bounding_boxes)
+            {
+                uint c_start = bounding_box[0];
+                uint r_start = bounding_box[1];
+                uint z_start = bounding_box[2];
+                uint c_stop = c_start + bounding_box[3];
+                uint r_stop = r_start + bounding_box[4];
+                uint z_stop = z_start + bounding_box[5];
+                NDarray dose_cube = dose_np[$"{z_start}:{z_stop},{r_start}:{r_stop},{c_start}:{c_stop}"];
+                NDarray gradient_np = np.abs(np.gradient(dose_cube, 2));
+                NDarray super_imposed_gradient_np = np.sum(gradient_np, 0);
+                NDarray min_gradient = np.min(super_imposed_gradient_np);
+                NDarray[] min_location = np.where(super_imposed_gradient_np <= min_gradient * 1.1);
+                VectorDouble continous_index = new VectorDouble();
+                uint min_z =  z_start + (uint)min_location[0][0];
+                uint min_row = r_start + (uint)min_location[1][0];
+                uint min_col = c_start + (uint)min_location[2][0];
+                continous_index.Add(min_col);
+                continous_index.Add(min_row);
+                continous_index.Add(min_z);
+                VectorDouble physical_location = dose_handle.TransformContinuousIndexToPhysicalPoint(continous_index);
+                int x = 1;
+            }
+            //bounding_boxes = np.asarray([truth_stats.GetBoundingBox(_) for _ in truth_stats.GetLabels()])
         }
     }
 }
