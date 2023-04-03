bde98a058eff3048f4a81f616666bebf672f3e89
brianmanderson <markba122@gmail.com>
2023-04-02 22:55:32 -0400

    turn the dose to sitk image into a modular project

--- StablePointFinder.cs
+++ StablePointFinder.cs
@@ -8,6 +8,7 @@ using VMS.TPS.Common.Model.Types;
 using SitkImage = itk.simple.Image;
 using itk.simple;
 using Stability.StablePointFinderClass;
+using System.Data;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
 [assembly: AssemblyVersion("1.0.0.9")]
@@ -67,6 +68,45 @@ namespace Stability.EsapiApp
         {
             SimpleITK.WriteImage(image, @"C:\Users\fpenaloza\Downloads\compared.nii.gz");
         }
+        static void write_image(SitkImage image, string location)
+        {
+            SimpleITK.WriteImage(image, location);
+        }
+        static SitkImage DoseToImage(Dose dose)
+        {
+            int W = dose.XSize;
+            int H = dose.YSize;
+            int D = dose.ZSize;
+            double sx = dose.XRes;
+            double sy = dose.YRes;
+            double sz = dose.ZRes;
+            VVector origin = dose.Origin;
+            VVector rowDirection = dose.XDirection;
+            VVector colDirection = dose.YDirection;
+            double xsign = rowDirection.x > 0 ? 1.0 : -1.0;
+            double ysign = colDirection.y > 0 ? 1.0 : -1.0;
+            double zsign = GetZDirection(rowDirection, colDirection).z > 0 ? 1.0 : -1.0;
+
+            SitkImage dose_handle = new SitkImage((uint)W, (uint)H, (uint)D, PixelIDValueEnum.sitkFloat32);
+            VectorDouble spacing = new VectorDouble() { sx * xsign, sy * ysign, sz * zsign };
+            VectorDouble sitk_origin = new VectorDouble() { origin.x, origin.y, origin.z };
+            dose_handle.SetSpacing(spacing);
+            dose_handle.SetOrigin(sitk_origin);
+            int[,] buffer = new int[W, H];
+            for (int z = 0; z < D; z++)
+            {
+                dose.GetVoxels(z, buffer);
+                for (int y = 0; y < H; y++)
+                {
+                    for (int x = 0; x < W; x++)
+                    {
+                        float value = (float)buffer[x, y];
+                        dose_handle.SetPixelAsFloat(new VectorUInt32() { (uint)x, (uint)y, (uint)z }, value);
+                    }
+                }
+            }
+            return dose_handle;
+        }
         static void Execute(Application app)
     {
             // TODO: Add your code here.
@@ -78,38 +118,7 @@ namespace Stability.EsapiApp
                 {
                     Dose dose = plan.Dose;
                     //int maxValueForScaling = FindMaxValue(dose);
-                    int W = dose.XSize;
-                    int H = dose.YSize;
-                    int D = dose.ZSize;
-                    double sx = dose.XRes;
-                    double sy = dose.YRes;
-                    double sz = dose.ZRes;
-                    VVector origin = dose.Origin;
-                    VVector rowDirection = dose.XDirection;
-                    VVector colDirection = dose.YDirection;
-                    double xsign = rowDirection.x > 0 ? 1.0 : -1.0;
-                    double ysign = colDirection.y > 0 ? 1.0 : -1.0;
-                    double zsign = GetZDirection(rowDirection, colDirection).z > 0 ? 1.0 : -1.0;
-
-                    SitkImage dose_handle = new SitkImage((uint)W, (uint)H, (uint)D, PixelIDValueEnum.sitkFloat32);
-                    VectorDouble spacing = new VectorDouble() { sx*xsign, sy*ysign, sz*zsign };
-                    VectorDouble sitk_origin = new VectorDouble() { origin.x, origin.y, origin.z };
-                    dose_handle.SetSpacing(spacing);
-                    dose_handle.SetOrigin(sitk_origin);
-                    int[,] buffer = new int[W, H];
-                    for (int z = 0; z < D; z++)
-                    {
-                        dose.GetVoxels(z, buffer);
-                        for (int y = 0; y < H; y++)
-                        {
-                            for (int x = 0; x < W; x++)
-                            {
-                                float value = (float)buffer[x, y];
-                                dose_handle.SetPixelAsFloat(new VectorUInt32() { (uint)x, (uint)y, (uint)z}, value);
-                            }
-                        }
-                    }
-                    int kk = 1;
+                    SitkImage dose_handle = DoseToImage(dose);
                     StablePointFinder finder = new StablePointFinder(dose_handle);
                     finder.execute();
                 }
