868fd6c45aa0cd704c90a5175608631371896bb1
brianmanderson <markba122@gmail.com>
2023-04-02 22:52:40 -0400

    rewritten as .NET console app, wasn't referencing the project correctly before

---
+++ Stability.ConsoleAppNet/Program.cs
@@ -0,0 +1,20 @@
+﻿using Stability.FinderClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stability.FinderClass;
+
+namespace Stability.ConsoleAppNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dose_file = @"C:\Users\markb\Modular_Projects\FindingStablePoints\dose.nii.gz";
+            StablePointFinder finder = new StablePointFinder(dose_file: dose_file);
+            finder.execute();
+        }
+    }
+}
